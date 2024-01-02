fa9d5c11b02023707c20bdeec098b5da1d43dd11
testingpoint09 <testingpoint09@gmail.com>
2024-01-02 13:26:53 +0530

    print statement is added

--- src/test/java/demopackage01/loginclass.java
+++ src/test/java/demopackage01/loginclass.java
@@ -1,5 +1,12 @@
 package demopackage01;
 
 public class loginclass {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Login class is calling...");
+		System.out.println("Login class is calling1...");
+	}
 
 }
