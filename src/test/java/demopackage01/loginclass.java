5bcb6a35b01dcbafedaf945d08f6de38c9e5d6fe
testingpoint09 <testingpoint09@gmail.com>
2024-01-02 13:32:46 +0530

    new lines of code added by

--- src/test/java/demopackage01/loginclass.java
+++ src/test/java/demopackage01/loginclass.java
@@ -7,6 +7,7 @@ public class loginclass {
 		
 		System.out.println("Login class is calling...");
 		System.out.println("Login class is calling1...");
+		System.out.println("Login class is calling2...");
 	}
 
 }
